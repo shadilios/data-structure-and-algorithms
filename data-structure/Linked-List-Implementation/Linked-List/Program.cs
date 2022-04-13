1e4cfbe850963291f9c6607152a6691226cdd6ad
shadilios <70282602+shadilios@users.noreply.github.com>
2022-04-13 08:04:00 +0300

    linked-list-kth-resubmit

--- data-structure/Linked-List-Implementation/Linked-List/Program.cs
+++ data-structure/Linked-List-Implementation/Linked-List/Program.cs
@@ -192,6 +192,63 @@ namespace Linked_List
 
         #endregion
 
+        #region Code Challenge 07 Part
+
+        public int Kth(int y)
+        {
+            //First i have to find how many elements i have [use counter]
+            //if the user inputs 0, I have to get back the last element
+            //if the user inputs 1, i have to get back the element before the last...and so on
+            //so length - input!
+
+            int key = y;
+            int firstCounter = 0;
+            int secondCounter = 0;
+            Node temp = headNode;
+            int result = 0;
+
+            //loop through list to find length
+            while (temp != null)
+            {
+                //check
+                firstCounter++;
+                temp = temp.next;
+            }
+
+            //reset
+            temp = headNode;
+
+            //loop again to specific element to get data
+            while (temp != null)
+            {
+                if (key > firstCounter -1)
+                {
+                    Console.WriteLine("Error, your key is bigger than the number of elements in the list, returning zero");
+                    break;
+                }
+
+                if (key < 0)
+                {
+                    Console.WriteLine("Error, your key can't be less than 0, returning zero");
+                    break;
+                }
+
+                secondCounter++;
+                if (secondCounter == (firstCounter - key))
+                {
+                    result = temp.data;
+                    break;
+                }
+                else
+                {
+                    temp = temp.next;
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
 
     }
 
@@ -207,13 +264,28 @@ namespace Linked_List
             list.Append(5);
 
             //list.InsertBefore(5, 10);
-            list.InsertAfter(1, 10);
-            list.InsertAfter(2, 10);
-            list.InsertAfter(3, 10);
-            list.InsertAfter(4, 10);
-            list.InsertAfter(5, 10);
+            //list.InsertAfter(1, 10);
+            //list.InsertAfter(2, 10);
+            //list.InsertAfter(3, 10);
+            //list.InsertAfter(4, 10);
+            //list.InsertAfter(5, 10);
 
             Console.WriteLine(list.ConvertToString());
+
+            Console.WriteLine(list.Kth(0));
+            Console.WriteLine(list.Kth(1));
+            Console.WriteLine(list.Kth(2));
+            Console.WriteLine(list.Kth(3));
+            Console.WriteLine(list.Kth(4));
+            Console.WriteLine(list.Kth(5));
+            Console.WriteLine(list.Kth(6));
+            Console.WriteLine(list.Kth(-6));
+
+
+
+
+
+
         }
     }
 }
