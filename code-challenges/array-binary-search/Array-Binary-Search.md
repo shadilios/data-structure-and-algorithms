9672c5654ca9b7ad61b36e635a53a65feac878b4
shadilios <70282602+shadilios@users.noreply.github.com>
2022-04-16 06:25:18 +0300

    Array binary search resubmission done

---
+++ code-challenges/array-binary-search/Array-Binary-Search.md
@@ -0,0 +1,17 @@
+# Array Binary Search
+
+### Description:
+
+In this challenge, we were asked without utilizing any of the built-in methods to create a method that takes a sorted array and a search key as arguments, then apply the binary search algorithm inside the method to return the index of our searched number if found, otherwise return -1.
+
+
+### Whiteboard Process
+
+![img](./binary-search.png)
+
+
+### Approach & effeciency
+
+Big O:   
+* Time: O(log n) because it uses the famous binary search algorithm.
+* Space: O(1) returning a single value.
---
+++ code-challenges/array-binary-search/Array-Binary-Search/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace array_binary_search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] myArray = { 11, 22, 33, 44, 55, 66, 77 };
+            
+            //exists and outputs 4
+            int key = 55;
+            Console.WriteLine(BinarySearch(myArray, key));
+
+
+            //doesn't exists and outputs -1
+            int key2 = 99;
+            Console.WriteLine(BinarySearch(myArray, key2));
+
+        }
+
+        static int BinarySearch(int[] sortedArr, int key)
+        {
+            //set 2 pointers left and right, and search values between them
+            //and change left and right values each time we don't find the element
+
+
+            //Assign left and right value
+            int left = 0;
+            int right = sortedArr.Length - 1;
+
+            //default value if it doesn't exist
+            int myIndex = -1;
+
+            //search if the value "key" is in the array
+            //if it exists in the array, return its index
+            //if not return -1
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+
+                if (key == sortedArr[middle])
+                {
+                    myIndex = middle;
+                    break;
+                }
+                else if (key > sortedArr[middle])
+                {
+                    left = middle + 1;
+                }
+                else if (key < sortedArr[middle])
+                {
+                    right = middle - 1;
+                }
+
+            }
+
+            return myIndex;
+
+        }
+    }
+}
