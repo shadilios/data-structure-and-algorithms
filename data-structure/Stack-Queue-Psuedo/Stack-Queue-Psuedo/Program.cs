4c343b0e370716b0feedf322b30efef9dc487e39
shadilios <70282602+shadilios@users.noreply.github.com>
2022-04-24 00:49:49 +0300

    Fixed error

--- data-structure/Stack-Queue-Psuedo/Stack-Queue-Psuedo/Program.cs
+++ data-structure/Stack-Queue-Psuedo/Stack-Queue-Psuedo/Program.cs
@@ -18,7 +18,9 @@ namespace Stack_Queue_Psuedo
             q.Enqueue(6);
             q.Enqueue(7);
 
-            //Console.WriteLine($"Queue Peek: {q.Peek()}");
+
+
+            Console.WriteLine($"Queue Peek: {q.Peek()}");
 
             Console.WriteLine("*******");
 
