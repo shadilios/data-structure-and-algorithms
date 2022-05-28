e45d1567c5133a91d7f12e7d27548a05c1335b81
shadilios <70282602+shadilios@users.noreply.github.com>
2022-05-28 16:01:47 +0300

    cc27 merge sort

---
+++ code-challenges/code27-merge-sort/Merge-Sort/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Merge_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 5, 2, 3, 9 };
+
+            MergeSort(x);
+
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+            
+        }
+
+
+
+        static int[] MergeSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            if (n > 1)
+            {
+                int mid = n / 2;
+                int[] left = arr[0..mid];
+                int[] right = arr[mid..n];
+
+                MergeSort(left);
+
+                MergeSort(right);
+
+                Merge(left, right, arr);
+
+            }
+
+            return arr;
+        }
+
+        static int[] Merge(int[] left, int[] right, int[] arr)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i = i + 1;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j = j + 1;
+                }
+
+                k = k + 1;
+            }
+
+            if (i == left.Length)
+            {
+                Array.Copy(right, j, arr, k, right.Length - j);
+            }
+            else
+            {
+                Array.Copy(left, i, arr, k, left.Length - i);
+            }
+
+            return arr;
+        }
+        
+
+    }
+
+
+
+
+
+
+
+
+
+}
