b45971df093fe561e2476313bd444f4f419f28f8
shadilios <70282602+shadilios@users.noreply.github.com>
2022-06-10 22:16:43 +0300

    Hash-table-implementation

---
+++ data-structure/hash-table/hash-table/HashTable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hash_table
+{
+    // Key: int
+    // Value: string
+
+    public class HashTable
+    {
+        public int size;
+
+        public Node[] hashTable;
+
+
+        public HashTable(int hashSize)
+        {
+            this.size = hashSize;
+            hashTable = new Node[hashSize];
+        }
+
+
+
+
+        public void Set(int key, string value)
+        {
+            int index = Hash(key);
+
+            if (hashTable[index] == null)
+            {
+              hashTable[index] = new Node(key, value);
+            }
+            else
+            {
+                Console.WriteLine($"Collision detected at {index}, replacing values");
+                hashTable[index].value = value;
+            }
+        }
+
+        public string Get(int key)
+        {
+            int index = Hash(key);
+
+            return hashTable[index].value;
+        }
+
+        public bool Contains(int key)
+        {
+            int index = Hash(key);
+
+            if (hashTable[index] != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public List<int>  Keys()
+        {
+            // loop over array
+            // if element is not null, take the key and add it to a list
+            // return the list
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < hashTable.Length; i++)
+            {
+                if (hashTable[i] != null)
+                {
+                    list.Add(hashTable[i].key);
+                }
+            }
+
+            return list;
+        }
+
+        public int Hash(int key)
+        {
+            // Last step:: % size => so that what ever index I get is within the size of the hash
+
+            return key % size;
+        }
+    }
+}
---
+++ data-structure/hash-table/hash-table/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hash_table
+{
+    public class Node
+    {
+        public int key;
+
+        public string value;
+
+        public Node(int key, string value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
