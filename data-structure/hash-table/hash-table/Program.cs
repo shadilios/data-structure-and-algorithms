9714de3152d8fbe4bef03410b8c59ddd3c6d0740
shadilios <70282602+shadilios@users.noreply.github.com>
2022-06-16 02:26:25 +0300

    hashmap-left-join

--- data-structure/hash-table/hash-table/Program.cs
+++ data-structure/hash-table/hash-table/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -9,18 +10,50 @@ namespace hash_table
     {
         static void Main(string[] args)
         {
-            HashTable ht = new HashTable(1024);
+            HashTable ht1 = new HashTable(1024);
 
-            ht.Set(0, "Shadi");
-            ht.Set(1, "Osama");
-            ht.Set(2, "Zaid");
-            ht.Set(3, "Ahmad");
-            ht.Set(4, "Laith");
+            ht1.Set(0, "Shadi");
+            ht1.Set(1, "Osama");
+            ht1.Set(2, "Zaid");
+            ht1.Set(3, "Ahmad");
+            ht1.Set(4, "Laith");
+
+            HashTable ht2 = new HashTable(1024);
+
+            ht2.Set(0, "Engineer");
+            ht2.Set(1, "Marketing");
+            ht2.Set(2, "Developer");
+            ht2.Set(3, "Support");
+            ht2.Set(4, "Something");
 
 
-            Console.WriteLine(ht.Get(2)); 
+
+
+            Console.WriteLine(ht1.Get(2));
+
+            LeftJoin(ht1, ht2);
+        }
+
+        public static void LeftJoin(HashTable left, HashTable right)
+        {
+            // Combine the key and corresponding values (if they exist) into a new data structure according to LEFT JOIN logic.
+            // LEFT JOIN means all the values in the first hashmap are returned, and if values exist in the “right” hashmap, they are appended to the result row.
+            // If no values exist in the right hashmap, then some flavor of NULL should be appended to the result row.
+
+            // This will Hold both of my tables joined together
+            List<string[]> arrayList = new List<string[]>();
+
+            for (int i = 0; i < left.hashTable.Length; i++)
+            {
+                string[] array = new string[3];
+
+                string word1 = Convert.ToString(i);
+                string word2 = left.Get(i);
+                string word3 = right.Get(i);
+
+                arrayList.Insert(i, array);
+            }
         }
 
     }
-
 }
