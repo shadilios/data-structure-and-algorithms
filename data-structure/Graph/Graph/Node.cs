e766ad5089cd35b40a01b0aa8bfc7b9ca4179312
shadilios <70282602+shadilios@users.noreply.github.com>
2022-06-21 05:50:24 +0300

    graph implementation not done yet

---
+++ data-structure/Graph/Graph/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    internal class Vertex
+    {
+        public int value;
+        public bool isVisited;
+        public List<Vertex> neighbours;
+
+        public Vertex(int value, List<Vertex> neighbours)
+        {
+            this.value = value;
+            isVisited = false;
+            this.neighbours = neighbours;
+        }
+
+        
+
+        
+    }
+}
